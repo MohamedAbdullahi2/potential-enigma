b549aebc880d0096a9ad85e2d5d7c8a0db4bda99
MohamedAbdullahi2 <118404373+MohamedAbdullahi2@users.noreply.github.com>
2023-02-02 22:48:16 -0500

    code ReadMe

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# nodejs project
+
+## Description
+Generate Professional ReadMe 
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+npm i
+
+## Usage
+information
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+fork respo and your code
+
+## Tests
+npm install --save-dev jest
+
+## Questions
+For questions, please contact me at moh.hassan2013@gmail.com.
---
+++ index.js
@@ -0,0 +1,101 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:'
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What is the license for this project?'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:'
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'Provide your contact information:'
+  }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`Successfully generated README.md`);
+  });
+}
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then(answers => {
+    const readme = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For questions, please contact me at ${answers.questions}.
+`;
+    writeToFile('README.md', readme);
+  });
+}
+
+// Function call to initialize app
+init();
+
+
